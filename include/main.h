fee49f547017536e8cf3edb820012401c59416b1
CapaaSimon <simon.simonise@gmail.com>
2025-10-29 14:16:22 +0100

    Redevelopment of the main, some messages to know where fonctions are

--- include/main.h
+++ include/main.h
@@ -1,7 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-// Declare that these variables exist "externally" (in main.c)
+// include these here so the App_Window struct understands the types
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+typedef struct {
+    // SDL components
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    TTF_Font* font;
+
+    // Application data
+    int* array;
+    
+    // Application state
+    int running;
+    int selectedAlgorithm;
+
+} App_Window;
+
+// Global constants (define in main.c)
 extern const int WINDOW_WIDTH;
 extern const int WINDOW_HEIGHT;
 extern const int N; // array size
--- include/visual.h
+++ include/visual.h
@@ -3,17 +3,31 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include "main.h"
 
+
+//Initializes all SDL/TTF systems and creates the app struct.
+// return a pointer to the newly created App_window, or NULL on failure.
+//implemented in visual.c
+App_Window* initAppVisuals(); // Renamed to be specific to 'visual'
+
+// Cleans up all resources (memory, SDL, TTF).
+//param app A pointer to the App_Window struct to be freed.
+//implemented in visual.c
+void cleanupAppVisuals(App_Window* app);
+
+//implemented in visual.c
 void drawArray(SDL_Renderer* renderer, int* array, int size, int highlight1, int highlight2);
 
 
- // (Clears screen, draws elements, and presents):
- // renderer :The SDL renderer to draw on.
- // array :The array of integers to visualize.
- // size :The size of the array.
-
+// (Clears screen, draws elements, and presents):
+// renderer :The SDL renderer to draw on.
+// array :The array of integers to visualize.
+// size :The size of the array.
+//implemented in visual.c
 void renderApp(SDL_Renderer* renderer, TTF_Font* font, int* array, int size, int highlight1, int hightlight2, int selectedAlgorithm);
 
+//implemented in visual.c
 void drawLegend(SDL_Renderer* renderer, TTF_Font* font, int selectedAlgorithm);
 
 #endif 
